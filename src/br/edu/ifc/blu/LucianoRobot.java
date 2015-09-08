8ebfb5d0203f85a84c277e5c12e2466302500d44
Luciano Jr <luciano@lucianojr.com.br>
2015-09-08 16:43:19 -0300

    Update LucianoRobot code and Sentinel

--- src/br/edu/ifc/blu/LucianoRobot.java
+++ src/br/edu/ifc/blu/LucianoRobot.java
@@ -1,39 +1,54 @@
 package br.edu.ifc.blu;
 
+import robocode.BulletHitBulletEvent;
+import robocode.BulletMissedEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
-/**
- * Created by luciano on 18/08/15.
- */
 public class LucianoRobot extends Robot {
     @Override
     public void run() {
         setColors(Color.BLUE, Color.BLACK, Color.blue, Color.RED, Color.CYAN);
-
-
-
         back(1000);
         setDebugProperty("Teste", Double.toString(getX()));
         setDebugProperty("Teste", Double.toString(getHeading()));
         setDebugProperty("Teste", Double.toString(getBattleFieldHeight()));
 
-
         while (getY() != getBattleFieldHeight()) {
-            turn
+            findEnemy();
         }
-        findEnemy();
     }
 
     private void findEnemy() {
         turnGunLeft(360);
+    }
 
+    @Override
+    public void onBulletMissed(BulletMissedEvent event) {
+        ahead(100);
+        turnRight(40);
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-        fire(100);
+        if ( ! isFrindy(event)) {
+            setDebugProperty("Nome", event.getName());
+            fire(100);
+        }
+
     }
+
+    private boolean isFrindy(ScannedRobotEvent event) {
+        return event.getName().equals("Luciano1Robot") || event.getName().equals("Luciano2Robot") || event.getName().equals("Luciano3Robot");
+    }
+
+    @Override
+    public void onBulletHitBullet(BulletHitBulletEvent event) {
+        turnRight(90);
+        ahead(100);
+    }
+
+
 }
